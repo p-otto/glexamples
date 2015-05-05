3143c9848d9dbd704452eb90973d3212bbfa0d31
Philipp Otto <philippotto.po@gmail.com>
2015-05-05 20:06:44 +0200

    create directory for ambient occlusion based on emptyexample

---
+++ source/ambientocclusion/AmbientOcclusion.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <memory>
+
+#include <glbinding/gl/types.h>
+
+#include <globjects/base/ref_ptr.h>
+
+#include <gloperate/painter/Painter.h>
+
+
+namespace globjects
+{
+    class Program;
+}
+
+namespace gloperate
+{
+    class AdaptiveGrid;
+    class Icosahedron;
+    class AbstractTargetFramebufferCapability;
+    class AbstractViewportCapability;
+    class AbstractPerspectiveProjectionCapability;
+    class AbstractCameraCapability;
+}
+
+class AmbientOcclusion : public gloperate::Painter
+{
+public:
+    AmbientOcclusion(gloperate::ResourceManager & resourceManager);
+    virtual ~AmbientOcclusion();
+
+    void setupProjection();
+
+protected:
+    virtual void onInitialize() override;
+    virtual void onPaint() override;
+
+protected:
+    /* capabilities */
+    gloperate::AbstractTargetFramebufferCapability * m_targetFramebufferCapability;
+    gloperate::AbstractViewportCapability * m_viewportCapability;
+    gloperate::AbstractPerspectiveProjectionCapability * m_projectionCapability;
+    gloperate::AbstractCameraCapability * m_cameraCapability;
+
+    /* members */
+    globjects::ref_ptr<gloperate::AdaptiveGrid> m_grid;
+    globjects::ref_ptr<gloperate::Icosahedron> m_icosahedron;
+    globjects::ref_ptr<globjects::Program> m_program;
+    gl::GLint m_transformLocation;
+};
---
+++ source/ambientocclusion/plugin.cpp
@@ -0,0 +1,15 @@
+#include <gloperate/plugin/plugin_api.h>
+
+#include "AmbientOcclusion.h"
+
+#include <glexamples-version.h>
+
+GLOPERATE_PLUGIN_LIBRARY
+
+    GLOPERATE_PLUGIN(AmbientOcclusion
+    , "AmbientOcclusion"
+    , "Screen-Space Ambient Occlusion Techniques"
+    , GLEXAMPLES_AUTHOR_ORGANIZATION
+    , "v1.0.0")
+
+GLOPERATE_PLUGIN_LIBRARY_END
